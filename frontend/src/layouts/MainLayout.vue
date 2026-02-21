153c221c33d35430f41d1b52823f54fd390845aa
kirabian <fabiansyahalghiffarireal@gmail.com>
2026-02-21 13:56:08 +0700

    anu

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -61,11 +61,8 @@ onErrorCaptured((err, instance, info) => {
       <!-- Page Content Wrapper -->
       <div class="flex-1 overflow-y-auto custom-scrollbar relative z-10 flex flex-col">
         <div class="p-4 md:p-8 pt-6 flex-1 flex flex-col">
-          <router-view v-slot="{ Component }">
-            <transition name="fade" mode="out-in">
-              <component :is="Component" :key="route.path" />
-            </transition>
-          </router-view>
+          <router-view />
+
         </div>
       </div>
     </main>
