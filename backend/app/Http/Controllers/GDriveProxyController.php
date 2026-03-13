4631ed24ebe8b9ae9cd7ecbd9b7f1774c4dde48f
kirabian <fabiansyahalghiffarireal@gmail.com>
2026-03-13 15:34:48 +0700

    anu

--- backend/app/Http/Controllers/GDriveProxyController.php
+++ backend/app/Http/Controllers/GDriveProxyController.php
@@ -18,22 +18,31 @@ class GDriveProxyController extends Controller
             'filename' => 'required|string',
         ]);
 
+        Log::info('GDrive Proxy: Starting upload for ' . $request->filename);
+
         $scriptUrl = 'https://script.google.com/macros/s/AKfycbxJ4V2A4uX7Ft_FFsrRCx9A86KA9ev0eWZTxcHlmR-lBFzjeJthLWVrN7mDDBFJfmyM/exec';
 
         try {
-            $response = Http::post($scriptUrl, [
-                'fileBase64' => $request->fileBase64,
-                'filename' => $request->filename,
-            ]);
+            $response = Http::timeout(60)
+                ->withOptions(['allow_redirects' => true])
+                ->post($scriptUrl, [
+                    'fileBase64' => $request->fileBase64,
+                    'filename' => $request->filename,
+                ]);
+
+            Log::info('GDrive Proxy Status: ' . $response->status());
 
             if ($response->successful()) {
+                Log::info('GDrive Proxy Success');
                 return response()->json($response->json());
             }
 
-            Log::error('GDrive Proxy Error: ' . $response->body());
+            Log::error('GDrive Proxy Error Status: ' . $response->status());
+            Log::error('GDrive Proxy Error Body: ' . $response->body());
+
             return response()->json([
                 'success' => false,
-                'error' => 'Failed to upload to Google Drive via proxy.'
+                'error' => 'Failed to upload to Google Drive via proxy. Status: ' . $response->status()
             ], 500);
 
         } catch (\Exception $e) {
