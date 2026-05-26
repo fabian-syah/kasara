c6085e1d43e6077532554737143334cce650c54c
kirabian <fabiansyahalghiffarireal@gmail.com>
2026-05-26 17:29:28 +0700

    fixing

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,6 +5,19 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// CORS headers for storage files (FrankenPHP serves static files before Laravel)
+$requestUri = $_SERVER['REQUEST_URI'] ?? '';
+if (str_starts_with($requestUri, '/storage/')) {
+    header('Access-Control-Allow-Origin: https://stokps.com');
+    header('Access-Control-Allow-Methods: GET, OPTIONS');
+    header('Access-Control-Allow-Headers: *');
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(204);
+        exit;
+    }
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
